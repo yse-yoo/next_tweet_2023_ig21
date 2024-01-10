a9ffda7613ec1abeb89a83d058079a9f89c3c848
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-10 11:24:40 +0900

    add Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from '@/app/components/Navbar'
 import type { Metadata } from 'next'
 import './globals.css'
 
@@ -14,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body>
+        <Navbar />
         <main className='flex min-h-screen flex-col p-5'>
           {children}
         </main>
