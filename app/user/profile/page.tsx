74832dcdaa837dfaa8fd888ee79c4d2820859ef0
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-17 11:21:17 +0900

    update

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,8 +1,16 @@
 "use client"
 
+import Image from "next/image";
+import imageMe from '@/public/images/me.png'
+
 const ProfilePage = () => {
     return (
         <div>
+            <Image 
+            src={imageMe}
+            className="h-36 w-36" 
+            alt=""
+            />
             <h1 className="text-2xl text-center font-bold">Profile</h1>
         </div>
     );
