615467af4ed8e32e12efa946e9608bba21c51536
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-17 11:43:26 +0900

    add regist page

---
+++ app/auth/regist/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { FaUser } from "react-icons/fa";
+
+const RegistPage = () => {
+    return (
+        <div className="mx-auto w-1/3">
+            <h1 className="flex text-2xl text-center font-bold">
+                <FaUser className="mt-1 me-3" />
+                Register
+            </h1>
+        </div>
+    );
+}
+
+export default RegistPage;
