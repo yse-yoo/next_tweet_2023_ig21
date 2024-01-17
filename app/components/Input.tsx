9d40edfb6867a109421c781a9d9fc7fa3061b0cb
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-17 11:53:18 +0900

    update

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -1,8 +1,21 @@
 "use client"
 
+const className = `
+            p-3
+            my-2
+            border-2
+            border-gray-200
+            rounded
+            w-full
+            focus:outline-none
+            focus:bg-white
+            focus:border-blue-500
+`
+
 const Input = () => {
+
     return (
-        <input type="text" className="border" />
+        <input type="text" className={className} />
     );
 }
 
