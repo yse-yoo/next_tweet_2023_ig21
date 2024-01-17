5cd8ee48f945fb37b070c7cb7f1f7df5bdca13a4
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-17 10:30:41 +0900

    update

--- app/components/NavbarLink.tsx
+++ app/components/NavbarLink.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 const NavbarLink = () => {
     return (
-        <div>
-            Enter
-        </div>
+        <Link
+            href="/user/profile"
+            className="hidden md:inline-block p-3 text-black"
+            >
+            Profile
+        </Link>
     );
 }
 
