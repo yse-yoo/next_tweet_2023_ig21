822bb308d359365e05fd125432c60838a6c4993d
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-17 10:15:08 +0900

    update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,11 +5,11 @@ const Navbar = () => {
     console.log('Navbar!!!')
     return (
         <nav>
-            <div className='flex px-5 py-3 border-b'>
+            <div className="flex px-5 py-3 border-b">
                 <div className="flex items-center mr-6">
                     <SiLoopback className="me-3" size="2em" />
-                    <Link href="/" className='ms-3 me-3'>
-                        <span className='self-center text-2xl font-semibold'>Next Tweet</span>
+                    <Link href="/" className="ms-3 me-3">
+                        <span className="self-center text-black text-2xl font-semibold">Next Tweet</span>
                     </Link>
                 </div>
 
