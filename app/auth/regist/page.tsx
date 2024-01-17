446aa2c7e12711b1abc194ff4e0b92c7cecee10f
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-17 12:10:59 +0900

    update

--- app/auth/regist/page.tsx
+++ app/auth/regist/page.tsx
@@ -12,9 +12,19 @@ const RegistPage = () => {
             </h1>
 
             <div>
-                <Input type="text" />
-                <Input type="email" />
-                <Input type="password" />
+                <Input type="text" placeholder="Your Name"/>
+                <Input type="email" placeholder="Email" />
+                <Input type="password" placeholder="Password" />
+            </div>
+
+            <div>
+                <button className="
+                w-full bg-black
+                text-white hover:bg-gray-800
+                py-2 px-4 my-3
+                rounded-lg">
+                    Sign up
+                </button>
             </div>
         </div>
     );
