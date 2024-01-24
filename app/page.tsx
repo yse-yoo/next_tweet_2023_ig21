7b951e69f5a9895d9b4acb7ff67f4b81f77afd73
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-24 10:54:27 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
 "use client"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { User, testUser } from "./models/User";
 import { Tweet } from "./models/Tweet";
+import { getTweets } from "./services/TweetService";
 
 export default function Home() {
   //テストユーザの取得
   const [user] = useState<User>(testUser);
   const [tweets, setTweets] = useState<Tweet[]>([]);
 
+  useEffect(() => {
+    (async () => {
+      if (user?.accessToken) {
+        //APIからTweetデータ取得
+        const data = await getTweets(user.accessToken);
+        console.log(data)
+        //データ設定
+        setTweets(data);
+      }
+    })();
+  }, [user])
+  
   return (
     <div>
       <textarea className="resize-none w-full h-24 border rounded-md p-2"></textarea>
