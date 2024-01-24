56ca65d5ae9a5f77d0a83158640b03955f69c03b
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-24 10:34:28 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 "use client"
 
+import { useState } from "react";
+import { User, testUser } from "./models/User";
+import { Tweet } from "./models/Tweet";
+
 export default function Home() {
-  console.log('Home!!!!')
+  //テストユーザの取得
+  const [user] = useState<User>(testUser);
+  const [tweets, setTweets] = useState<Tweet[]>([]);
+
   return (
     <div>
       <textarea className="resize-none w-full h-24 border rounded-md p-2"></textarea>
