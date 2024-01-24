41c35f5d3b685e014fcbf92ac50d529c7f3fa5ea
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-01-24 11:47:45 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { User, testUser } from "./models/User";
 import { Tweet } from "./models/Tweet";
 import { getTweets } from "./services/TweetService";
 import TweetForm from "./components/tweet/TweetForm";
+import TweetList from "./components/tweet/TweetList";
 
 export default function Home() {
   //テストユーザの取得
@@ -28,7 +29,8 @@ export default function Home() {
       {
         user?.id > 0 && (
           <>
-          <TweetForm />
+            <TweetForm />
+            <TweetList tweets={tweets} />
           </>
         )
       }
